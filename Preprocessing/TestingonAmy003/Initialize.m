5f9b3d3dd34016ac78deb501a42c666c7f5773fe
Alireza Kazemi <alireza.kzmi@gmail.com>
2025-03-21 15:34:40 -0600

    Update

--- Preprocessing/TestingonAmy003/Initialize.m
+++ Preprocessing/TestingonAmy003/Initialize.m
@@ -2,5 +2,20 @@
 
 rdDir = uigetdir([],"Choose Reading Directory");
 rdDir = string(rdDir) + string(filesep);
-wrDir = uigetdir(readDir,"Choose Writing Directory");
+wrDir = uigetdir(rdDir,"Choose Writing Directory");
 wrDir = string(wrDir) + string(wrDir);
+
+
+if isempty(which('chronux')) % Check if Chronux is in the path
+    disp('Chronux package not found in the path.');
+    folderPath = uigetdir(pwd, 'Select the Chronux package folder');
+    if folderPath ~= 0 % If the user selects a folder
+        addpath(genpath(folderPath));
+        savepath; % Save the updated path
+        disp('Chronux package added to the path.');
+    else
+        disp('Chronux package not added. Please ensure it is accessible.');
+    end
+else
+    disp('Chronux package is already in the path.');
+end
