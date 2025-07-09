e934bc5ac08c042ebab54fe0594c3098fa575b20
Martina Krisztina Hollearn <77700935+mkhollearn@users.noreply.github.com>
2025-07-09 12:04:16 -0600

    Create L5_ExtractConditions.m
    added freq value names to output csv

---
+++ MatlabPipeline/L5_ExtractConditions.m
@@ -0,0 +1,74 @@
+%% Initialization
+clear;
+clc;
+%-----------Raw Data Path
+RDD = "Z:\Data\AMME_Data_Emory\AMME_Data\";
+%-----------Read Processed Data from:
+RD = "D:\Individuals\Martina\Data\Amme\MatlabPipeline\";
+%-----------Write Processed Data to:
+WR = "D:\Individuals\Martina\Data\Amme\MatlabPipeline\CSVOutput\";
+
+%################################ Add Required Toolboxes
+ChronuX_path = "D:\Toolboxes\chronux_2_12";
+addpath(genpath(ChronuX_path));
+
+%################################ load patient Structure
+load(RD+"PatientStructL4NoEEG.mat");
+
+%DisplayPatientStructInfo(patient);
+
+%% Parameters
+% This routine extracts processed data for specified conditions from patient 
+% structure for visualization or statistical analysis or exporting data.
+%################################ Parameters
+% Frequency 
+par.Freqs = [0,100];
+par.PatientList = [1:9,11:24];
+par.Measure = "Coherence";
+par.Region = ["HPC","PHG","BLA"];
+par.Phase = 3;
+par.ChannelOrder = "1_1"; % Don't change this for Coherency use "1_1"
+
+%% Extract Results
+for pIdx = par.PatientList
+    dat = table;
+    for rgIdx = 1:length(par.Region)
+        phIdx = par.Phase;
+    
+        datRes = struct2table(patient(pIdx).phase(phIdx).trial);
+        result = patient(pIdx).phase(phIdx).Results;
+        result = result(result.Measure == par.Measure,:);
+        result = result(result.ChOrder == par.ChannelOrder,:);
+        result = result(result.Region == par.Region(rgIdx),:);
+        if(size(result,1)==0)
+            continue;
+        end
+
+        measureIdx = result.EpochTime=="PreImage";
+        freqIdx = result.Freqs{measureIdx}>=par.Freqs(1)  & result.Freqs{measureIdx}<=par.Freqs(2);
+        freqVals = result.Freqs{measureIdx}(freqIdx);
+        values = result.Value{measureIdx};
+        values1 = values(freqIdx,:)';
+    
+        measureIdx = result.EpochTime=="PostImage";
+        freqIdx = result.Freqs{measureIdx}>=par.Freqs(1)  & result.Freqs{measureIdx}<=par.Freqs(2);
+        values = result.Value{measureIdx};
+        values2 = values(freqIdx,:)';
+    
+        values = values2-values1;% Values Already converted to db for power and fisher-z transformed for coherency
+    
+        datRes.Patient = repmat(string(patient(pIdx).name), size(datRes,1),1);
+        datRes.Phase = repmat(phIdx, size(datRes,1),1);
+        datRes.Measure = repmat(par.Measure, size(datRes,1),1);
+        datRes.Region = repmat(par.Region(rgIdx), size(datRes,1),1);
+
+        % Assign frequency-labeled columns
+        datRes(:, strcat("Freq_", string(freqVals))) = array2table(values);
+
+        dat = cat(1,dat,datRes);
+    end
+    % writetable(datRes,WR+"patient"+pIdx+"phase"+phIdx+"Measure"+par.Measure+".csv")
+    writetable(datRes,WR+string(patient(pIdx).name)+"phase"+phIdx+"_1-100Hz"+par.Measure+".csv")
+end
+%save(WR+"FreqValsfor"+"_phase"+phIdx+"_Measure","freqVals");
+
