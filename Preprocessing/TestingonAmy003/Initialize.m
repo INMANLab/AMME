53161b6f6e63df3a1274c6d44f4174a47f8a6144
Alireza Kazemi <alireza.kzmi@gmail.com>
2025-08-13 09:41:01 -0600

    Update Initialize.m

--- Preprocessing/TestingonAmy003/Initialize.m
+++ Preprocessing/TestingonAmy003/Initialize.m
@@ -1,11 +1,19 @@
 %% Initialize codes running from Github to get required directories
 
-rdDir = uigetdir([],"Choose Reading Directory");
-rdDir = string(rdDir) + string(filesep);
-wrDir = uigetdir(rdDir,"Choose Writing Directory");
-wrDir = string(wrDir) + string(wrDir);
+% --- Set Path Variables ---
+% Get the raw data directory, starting the search in the reading directory
+rawDataDir = uigetdir(rdDir, "Choose Raw Data Directory");
+rawDataDir = string(rawDataDir) + filesep;
 
+% Get the primary reading directory
+rdDir = uigetdir([], "Choose Reading Directory");
+rdDir = string(rdDir) + filesep;
 
+% Get the writing directory, starting the search in the reading directory
+wrDir = uigetdir(rdDir, "Choose Writing Directory");
+wrDir = string(wrDir) + filesep; % Corrected this line
+
+% --- Check for Chronux Toolbox ---
 if isempty(which('chronux')) % Check if Chronux is in the path
     disp('Chronux package not found in the path.');
     folderPath = uigetdir(pwd, 'Select the Chronux package folder');
